c2c8fc2ae4b32acbd087a5b4f1655cbf438392f3
dp <dpronin@yandex.ru>
2019-12-04 23:34:31 -0800

    tests added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,63 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ttt/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Display TicTacToe field', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Main());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // 9 empty squares on the screen.
+    expect(find.byIcon(Icons.fullscreen), findsNWidgets(9));
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Should win', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(Main());
+
+    // Tap the first tile.
+    await tester.tap(find.byIcon(Icons.fullscreen).at(0));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(1));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(3));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(4));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(6));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Wins"), findsOneWidget);
+  });
+
+  testWidgets('Should not win', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(Main());
+
+    // Tap the first tile.
+    await tester.tap(find.byIcon(Icons.fullscreen).at(0));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(1));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(3));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(4));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(7));
+    await tester.pump();
+
+    expect(find.text("Wins"), findsNothing);
+  });
+
+  testWidgets('Should draw', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(Main());
+
+    // Tap the first tile.
+    await tester.tap(find.byIcon(Icons.fullscreen).at(0));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(1));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(3));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(4));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(7));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(6));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(2));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(5));
+    await tester.tap(find.byIcon(Icons.fullscreen).at(8));
+    await tester.pump();
+
+    expect(find.text("Draw"), findsOneWidget);
   });
 }
